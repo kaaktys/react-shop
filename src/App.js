ccf1b302f3b8f09c63144d470c5fe689d8e634b2
unknown <katok0709@gmail.com>
2020-04-28 20:25:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { isValidElement } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+
+      <div className="header">
+        Header
+      </div>
+      <div className="menu">
+          <a href="">Главная</a>
+          <a href="">Каталог</a>
+          <a href="">Доставка и оплата</a>
+          <a href="">Прайс-лист</a>
+          <a href="">Контакты</a>
+        </div>
+
+        <div className="slider">
+          <div className="prev-btn">
+            <button>prev</button>
+          </div>
+          <div className="next-btn">
+            <button>next</button>
+          </div>
+          <div>
+            <img src="https://ramki-vsem.ru/fon/raznocvetnyj-fon18.jpg" alt="" />
+          </div>
+        </div>
+
+
+
+        <div className="nav"> Навбар
+          <ul>
+            <li><a href="Бытовая техника">Бытовая техника</a></li>
+            <li><a href="Мобильная техника">Мобильная техника</a></li>
+            <li><a href="Компьютеры">Компьютеры</a></li>
+            <li><a href="Одежда">Одежда</a></li>
+            <li><a href="Недвижимость">Недвижимость</a></li>
+          </ul>
+        </div>
+      <div className="content">
+        Content
+      </div>
+      <div className="footer">
+        Footer
+      </div>
+
     </div>
   );
 }
