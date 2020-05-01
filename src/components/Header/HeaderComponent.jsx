72938816298273af077bf77cff38a54a3f011585
unknown <katok0709@gmail.com>
2020-05-01 15:16:18 +0300

    trash

---
+++ src/components/Header/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './HeaderComponent.css'
+import logo from '../../assets/logo.png';
+
+const HeaderComponent = () => {
+    return (
+        <div className='header'>
+            <img src={logo} />
+            <h3>Название компании</h3>
+            <p>самая клевая компания</p>
+        </div>
+    )
+}
+
+export default HeaderComponent
