9670edd9ad19421b209948d6b2634ab5905bbf6c
unknown <xin90210@gmail.com>
2020-05-09 16:43:55 +0300

    header finished

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import './HeaderComponent.scss'
-// import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.png';
 
 const HeaderComponent = () => {
     return (
         <header className="header">
-            <div className="title-group">
-                <img src="" />
-                <div>
-                    <h3>Название компании</h3>
-                    <p>самая клевая компания</p>
+            <div className="header-content">
+                <div className="title-group">
+                    <img src={logo} />
+                    <div>
+                        <h3>Название компании</h3>
+                        <p>самая клевая компания</p>
+                    </div>
                 </div>
-            </div>
 
-            <div className="contact-group">
-                <a href="tel:+74998888888">+7(499)888-88-88</a>
-                <a href="tel:+74997777777">+7(499)777-77-77</a>
-                <a className="feedback">Обратная связь</a>
+                <div className="contact-group">
+                    <a href="tel:+74998888888">+7(499) 888-88-88</a>
+                    <a href="tel:+74997777777">+7(499) 777-77-77</a>
+                    <a className="feedback">Обратная связь</a>
+                </div>
             </div>
         </header>
     )
